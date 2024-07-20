bcc3e6f92c8b341f2e8a5b62bceb3e8fcfa135fa
bhargavivennam <51819569+bhargavivennam@users.noreply.github.com>
2024-07-20 15:41:18 -0500

    API Gateway feature to authenticate incoming requests and redirect to KeyCloak

--- src/main/java/com/consultingfirm/api_gateway_service/ApiGatewayConfig.java
+++ src/main/java/com/consultingfirm/api_gateway_service/ApiGatewayConfig.java
@@ -5,16 +5,19 @@ import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin
 public class ApiGatewayConfig {
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(p -> p.path("/api/users/**")
+                .route(p -> p.path("/test/**","/swagger-ui/**","/v3/api-docs/**")
                         .filters(GatewayFilterSpec::tokenRelay)
-                        .uri("http://localhost:9090")) //user details microservice port number
+                        .uri("http://localhost:9091")) //user details microservice port number
+//                        .uri("lb://subdrive-rentals"))
                 .build();
     }
 }
